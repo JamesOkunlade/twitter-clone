50cf84964286230044f5cf76416fa9747bc65466
James Okunlade <james.okunlade@gmail.com>
2020-08-07 20:51:17 +0100

    follower and followings model reverted

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweeets
+  has_many :followers
+  has_many :followings
+  # has_many :followships, 
+  # has_many :followings, through: :followships, source: :followee
+  # has_many :followers, through: :followships, source: :follower
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+
+  # post 'follow/:user_id', to: 'follows#follow', as: 'follow'
+  # delete 'unfollow/:user_id' to: 'follows#unfollow', as: 'unfollow'
+
+
   resources :tweeets
+  resources :follows, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "tweeets#index"
 end
